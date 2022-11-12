ad96c192b2e0809b428a96ade6ee9d90dd7e6a47
triplek <triplek@triplek.net>
2022-11-12 16:22:04 +0900

    #20.14 useMutation

--- src/api.ts
+++ src/api.ts
@@ -69,3 +69,35 @@ export const kakaoLogIn = (code: string) =>
       }
     )
     .then((response) => response.status);
+
+/**
+ * Username login
+ */
+export interface IUsernameLoginVariables {
+  username: string;
+  password: string;
+}
+
+export interface IUsernameLoginSuccess {
+  ok: string;
+}
+
+export interface IUsernameLoginError {
+  error: string;
+}
+
+export const usernameLogIn = ({
+  username,
+  password,
+}: IUsernameLoginVariables) =>
+  axiosInstance
+    .post(
+      `/users/log-in/`,
+      { username, password },
+      {
+        headers: {
+          "X-CSRFToken": Cookie.get("csrftoken") || "",
+        },
+      }
+    )
+    .then((response) => response.data);
--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,4 +1,6 @@
+import { useForm } from "react-hook-form";
 import {
+  Box,
   Button,
   HStack,
   Input,
@@ -10,66 +12,108 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { FaLock, FaUserCircle } from "react-icons/fa";
 import SocialLogin from "./SocialLogin";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  IUsernameLoginError,
+  IUsernameLoginSuccess,
+  IUsernameLoginVariables,
+  usernameLogIn,
+} from "../api";
 
 interface LoginModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+interface IForm {
+  username: string;
+  password: string;
+}
+
 export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
-  const [username, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const onChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    const { name, value } = e.currentTarget;
-    if (name === "username") {
-      setUserName(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+  const toast = useToast();
+  const queryClient = useQueryClient();
+  const mutation = useMutation<
+    IUsernameLoginSuccess,
+    IUsernameLoginError,
+    IUsernameLoginVariables
+  >(usernameLogIn, {
+    onMutate: () => {
+      console.log("mutation starting.");
+    },
+    onSuccess: (data) => {
+      toast({
+        title: "Welcome back!",
+        status: "success",
+        variant: "left-accent",
+        position: "top",
+      });
+      onClose();
+      queryClient.refetchQueries(["me"]);
+    },
+    onError: (error) => {
+      console.log("mutation has an error.");
+    },
+  });
+  const onSubmit = ({ username, password }: IForm) => {
+    mutation.mutate({ username, password });
   };
-  const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(username, password);
-  };
+
   return (
     <Modal onClose={onClose} isOpen={isOpen}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Log in</ModalHeader>
         <ModalCloseButton aria-label="Close login layer" />
-        <ModalBody as={"form"} onSubmit={onSubmit as any}>
+        <ModalBody as={"form"} onSubmit={handleSubmit(onSubmit)}>
           <VStack>
             <InputGroup>
               <InputLeftElement children={<FaUserCircle />} />
               <Input
-                name="username"
-                onChange={onChange}
+                isInvalid={Boolean(errors.username?.message)}
+                {...register("username", {
+                  required: "Please enter your name.",
+                })}
                 variant={"filled"}
                 placeholder="Username"
-                value={username}
-                required
               />
             </InputGroup>
+            {errors.username?.message ? (
+              <Box role={"alert"} w={"100%"} color={"red.600"}>
+                {errors.username.message}
+              </Box>
+            ) : null}
             <InputGroup>
               <InputLeftElement children={<FaLock />} />
               <Input
-                name="password"
-                onChange={onChange}
+                isInvalid={Boolean(errors.password?.message)}
+                {...register("password", {
+                  required: "Please enter your password.",
+                })}
                 variant={"filled"}
                 type={"password"}
                 placeholder="Password"
-                value={password}
-                required
               />
             </InputGroup>
           </VStack>
           <HStack mt={"4"}>
-            <Button type={"submit"} colorScheme={"red"} width={"100%"}>
+            <Button
+              isLoading={mutation.isLoading}
+              type={"submit"}
+              colorScheme={"red"}
+              width={"100%"}
+            >
               Log in
             </Button>
           </HStack>
